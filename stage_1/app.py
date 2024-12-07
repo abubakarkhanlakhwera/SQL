8d203b38ce6c2d0803dbb56770b2c300214553e0
Abubakar khan Lakhwera <109948432+abubakarkhanlakhwera@users.noreply.github.com>
2024-12-07 21:26:47 +0500

    data base

--- stage_1/app.py
+++ stage_1/app.py
@@ -19,8 +19,35 @@ class Flipkart:
             self.login()
         else:
             sys.exit(1000)
+    def login_menu(self):
+        user_input = input("""
+        1. Enter 1 to see profile
+        2. Enter 2 to edit profile
+        3. Enter 3 to delete profile
+        4. Enter 4 to logout
+        """)
+
     def register(self):
-        pass
+        name = input("Enter your name: ")
+        email = input("Enter your email: ")
+        password = input("Enter your password: ")
+        response = self.db.register(name,email,password)
+        if response == 1:
+            print("Registration successful")
+
+        else:
+            print("Registration failed")
+        self.menu()
+
     def login(self):
-        pass
+        email = input("Enter your email: ")
+        password = input("Enter your password: ")
+        data = self.db.search(email,password)
+
+        if len(data) == 0:
+            print("Invalid credentials")
+            self.login()
+        else:
+            print('Hello',data[0][1])
+            self.login_menu()
 obj = Flipkart()
--- stage_1/db_helper.py
+++ stage_1/db_helper.py
@@ -10,3 +10,25 @@ class DBhelper:
             print('Connection failed')
         else:
             print(f'Connected to  database')
+
+    def register(self,name,email,password):
+        try:
+            self.mycursor.execute("""
+            INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES (NULL, '{}', '{}', '{}')
+            """.format(name,email,password))
+            self.con.commit()
+        except:
+            return  -1
+        else:
+            return 1
+    def search(self,email,password):
+        try:
+            self.mycursor.execute("""
+            SELECT * FROM users WHERE email LIKE '{}' AND password LIKE '{}' 
+            """.format(email,password))
+            data = self.mycursor.fetchall()
+            return data
+        except:
+            print('error')
+
+
